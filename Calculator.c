df01b1644e55972260aa10a2870f87c831f60cc0
Anurag Deo <anuragdeo03@gmail.com>
2022-07-27 08:24:04 +0530

    Added the exit option and code formatting

--- Calculator.c
+++ Calculator.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 
-int add(int a, int b) {
+int add(int a, int b)
+{
     return a + b;
 }
 
-int subtract(int a, int b) {
+int subtract(int a, int b)
+{
     return a - b;
 }
 
-int multiply(int a, int b) {
+int multiply(int a, int b)
+{
     return a * b;
 }
 
-int divide(int a, int b) {
+int divide(int a, int b)
+{
     return a / b;
 }
 
-int main(){
+int main()
+{
     printf("Please choose an operation among the following:\n");
     printf("1. Addition\n");
     printf("2. Subtraction\n");
@@ -25,34 +30,38 @@ int main(){
     printf("5. Exit\n");
     int choice;
     scanf("%d", &choice);
-    switch(choice){
-        case 1:
-            printf("Please enter two numbers to add:\n");
-            int num1, num2;
-            scanf("%d %d", &num1, &num2);
-            printf("The sum of %d and %d is %d\n", num1, num2, add(num1, num2));
-            break;
-        case 2:
-            printf("Please enter two numbers to subtract:\n");
-            int num3, num4;
-            scanf("%d %d", &num3, &num4);
-            printf("The difference of %d and %d is %d\n", num3, num4, subtract(num3, num4));
-            break;
-        case 3:
-            printf("Please enter two numbers to multiply:\n");
-            int num5, num6;
-            scanf("%d %d", &num5, &num6);
-            printf("The product of %d and %d is %d\n", num5, num6, multiply(num5, num6));
-            break;
-        case 4:
-            printf("Please enter two numbers to divide:\n");
-            int num7, num8;
-            scanf("%d %d", &num7, &num8);
-            printf("The quotient of %d and %d is %d\n", num7, num8, divide(num7, num8));
-            break;
-        default:
-            printf("Invalid choice. Exiting...\n");
-            break;
+    switch (choice)
+    {
+    case 1:
+        printf("Please enter two numbers to add:\n");
+        int num1, num2;
+        scanf("%d %d", &num1, &num2);
+        printf("The sum of %d and %d is %d\n", num1, num2, add(num1, num2));
+        break;
+    case 2:
+        printf("Please enter two numbers to subtract:\n");
+        int num3, num4;
+        scanf("%d %d", &num3, &num4);
+        printf("The difference of %d and %d is %d\n", num3, num4, subtract(num3, num4));
+        break;
+    case 3:
+        printf("Please enter two numbers to multiply:\n");
+        int num5, num6;
+        scanf("%d %d", &num5, &num6);
+        printf("The product of %d and %d is %d\n", num5, num6, multiply(num5, num6));
+        break;
+    case 4:
+        printf("Please enter two numbers to divide:\n");
+        int num7, num8;
+        scanf("%d %d", &num7, &num8);
+        printf("The quotient of %d and %d is %d\n", num7, num8, divide(num7, num8));
+        break;
+    case 5:
+        printf("Exiting...\n");
+        break;
+    default:
+        printf("Invalid choice. Exiting...\n");
+        break;
     }
     return 0;
 }
