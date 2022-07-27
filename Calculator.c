ad6bb53ad4ebdc72f3a0e3d43dfcec4d8bb354c2
Anurag Deo <anuragdeo03@gmail.com>
2022-07-27 08:22:35 +0530

    Added the division function

--- Calculator.c
+++ Calculator.c
@@ -12,6 +12,10 @@ int multiply(int a, int b) {
     return a * b;
 }
 
+int divide(int a, int b) {
+    return a / b;
+}
+
 int main(){
     printf("Please choose an operation among the following:\n");
     printf("1. Addition\n");
@@ -39,6 +43,12 @@ int main(){
             int num5, num6;
             scanf("%d %d", &num5, &num6);
             printf("The product of %d and %d is %d\n", num5, num6, multiply(num5, num6));
+            break;
+        case 4:
+            printf("Please enter two numbers to divide:\n");
+            int num7, num8;
+            scanf("%d %d", &num7, &num8);
+            printf("The quotient of %d and %d is %d\n", num7, num8, divide(num7, num8));
             break;
         default:
             printf("Invalid choice. Exiting...\n");
